ad6a5c8eb93650f440e3324657c4166486723b73
Kyle Yang <KyleY9@nycstudents.net>
2023-10-29 12:20:33 -0400

    Didn't really add much, I mainly just added and deleted code as I went along with the lab.

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,15 @@
-
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner myScanner = new Scanner(System.in);
-        System.out.print("Enter a lowercase word: ");
-        String word = myScanner.nextLine();
-        String first = word.substring(0, 1);  // get first letter
+        int p = 5;
+        int q = 0;
 
-        if (first.equals("a") || first.equals("e") || first.equals("i") || first.equals("o") || first.equals("u")) {
-            System.out.println("Your word begins with a vowel");
-        } else {
-            System.out.println("Your word begins with a consonant");
+        if (p > 0 && (p / q) == 2)
+        {
+            System.out.println("it’s true :)");
         }
-
-        myScanner.close();
+        else
+        {
+            System.out.println("it’s false :(");
+        }
     }
 }
